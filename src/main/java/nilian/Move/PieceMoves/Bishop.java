ec7ac0297569c32c6aa3ffdd3dcdf285b3203ce1
nilian <scaleenor@gmail.com>
2024-09-03 17:04:21 +0330

    [UPDATE] added bishop moves with bug!

--- src/main/java/nilian/Move/PieceMoves/Bishop.java
+++ src/main/java/nilian/Move/PieceMoves/Bishop.java
@@ -1,17 +1,100 @@
 package nilian.Move.PieceMoves;
 
 import nilian.Move.Coordinate;
+import nilian.board.BoardMaker;
 import nilian.board.ChessSquare;
+import nilian.board.Piece;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class Bishop
-{
+public class Bishop {
     /**
-     *  calculates all the possible coordinates as destination the bishop can go without getting the king killed.
+     * calculates all the possible coordinates as destination the bishop can go without getting the king killed.
+     *
      * @return a list of possible coordinates
      */
     public static List<Coordinate> calculatePossibleMoves(ChessSquare square) {
-        return List.of();
+        System.out.println("HORSE MOVE IS CALLED");
+        List<Coordinate> result = new ArrayList<>();
+        int i = square.getI();
+        int j = square.getJ();
+        Coordinate source = new Coordinate(i, j);
+        Coordinate destination;
+
+        //right up moves
+
+        for (int ei = 0; ei < 8; ei++) {
+            if (i - ei >= 0 && j + ei < 8)//validation
+            {
+                destination = new Coordinate(i - ei, j + ei);
+                if (BoardMaker.theBoard.getSquare(destination).getPiece() == Piece.EMPTY)
+                {
+                    if (MoveValidation.isThisMoveValid(source, destination))
+                        result.add(destination);
+                }
+                else if (BoardMaker.theBoard.getSquare(destination).getPieceColor() != square.getPieceColor())
+                {
+                    if (MoveValidation.isThisMoveValid(source, destination))
+                        result.add(destination);
+                }
+            } else {
+                break;
+            }
+        }
+        //right down moves
+        for (int ei = 0; ei < 8; ei++) {
+            if (i + ei < 8 && j + ei < 8)//validation
+            {
+                destination = new Coordinate(i + ei, j + ei);
+                if (BoardMaker.theBoard.getSquare(destination).getPieceColor() != square.getPieceColor())
+                {
+                    if (MoveValidation.isThisMoveValid(source, destination))
+                        result.add(destination);
+                } else if (BoardMaker.theBoard.getSquare(destination).getPiece() == Piece.EMPTY)
+                {
+                    if (MoveValidation.isThisMoveValid(source, destination))
+                        result.add(destination);
+                }
+            } else {
+                break;
+            }
+        }   //left up moves
+        for (int ei = 0; ei < 8; ei++) {
+            if (i - ei >= 0 && j - ei >= 0)//validation
+            {
+                destination = new Coordinate(i - ei, j - ei);
+                if (BoardMaker.theBoard.getSquare(destination).getPieceColor() != square.getPieceColor())
+                {
+                    if (MoveValidation.isThisMoveValid(source, destination))
+                        result.add(destination);
+                }
+                else if (BoardMaker.theBoard.getSquare(destination).getPiece() == Piece.EMPTY)
+                {
+                    if (MoveValidation.isThisMoveValid(source, destination))
+                        result.add(destination);
+                }
+            } else {
+                break;
+            }
+        }   //left down moves
+        for (int ei = 0; ei < 8; ei++) {
+            if (i + ei < 8 && j - ei >= 0)//validation
+            {
+                destination = new Coordinate(i + ei, j - ei);
+                if (BoardMaker.theBoard.getSquare(destination).getPieceColor() != square.getPieceColor())
+                {
+                    if (MoveValidation.isThisMoveValid(source, destination))
+                        result.add(destination);
+                } else if (BoardMaker.theBoard.getSquare(destination).getPiece() == Piece.EMPTY)
+                {
+                    if (MoveValidation.isThisMoveValid(source, destination))
+                        result.add(destination);
+                }
+            } else {
+                break;
+            }
+        }
+        return result;
     }
 }
