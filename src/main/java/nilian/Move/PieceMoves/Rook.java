8cfa5327886d74d017e50f25516e6d623aee2970
Nilian <scaleenor@gmail.com>
2024-09-04 10:07:58 +0330

    [UPDATE] Rook & Queen work fine

--- src/main/java/nilian/Move/PieceMoves/Rook.java
+++ src/main/java/nilian/Move/PieceMoves/Rook.java
@@ -1,8 +1,11 @@
 package nilian.Move.PieceMoves;
 
 import nilian.Move.Coordinate;
+import nilian.board.BoardMaker;
 import nilian.board.ChessSquare;
+import nilian.board.Piece;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Rook
@@ -12,6 +15,91 @@ public class Rook
      * @return a list of possible coordinates
      */
     public static List<Coordinate> calculatePossibleMoves(ChessSquare square) {
-        return List.of();
+        System.out.println("ROOK MOVE IS CALLED");
+        List<Coordinate> result = new ArrayList<>();
+        int i = square.getI();
+        int j = square.getJ();
+        Coordinate source = new Coordinate(i, j);
+        Coordinate destination;
+
+        //right moves
+        for (int ei = 1; ei < 8; ei++) {
+            if (j + ei < 8)
+            {
+                destination = new Coordinate(i, j + ei);
+                if (BoardMaker.theBoard.getSquare(destination).getPiece() == Piece.EMPTY)
+                {
+                    if (MoveValidation.isThisMoveValid(source, destination))
+                        result.add(destination);
+                } else {
+                    if (BoardMaker.theBoard.getSquare(destination).getPieceColor() != square.getPieceColor()) {
+                        if (MoveValidation.isThisMoveValid(source, destination))
+                            result.add(destination);
+                    }
+                    break;
+                }
+            } else {
+                break;
+            }
+        }
+        //left moves
+        for (int ei = 1; ei < 8; ei++) {
+            if (j - ei >= 0)//validation
+            {
+                destination = new Coordinate(i, j - ei);
+                if (BoardMaker.theBoard.getSquare(destination).getPiece() == Piece.EMPTY) {
+                    if (MoveValidation.isThisMoveValid(source, destination))
+                        result.add(destination);
+                }
+                else {
+                    if(BoardMaker.theBoard.getSquare(destination).getPieceColor() != square.getPieceColor()) {
+                        if (MoveValidation.isThisMoveValid(source, destination))
+                            result.add(destination);
+                    }
+                    break;
+                }
+            } else {
+                break;
+            }
+        }   //up moves
+        for (int ei = 1; ei < 8; ei++) {
+            if (i - ei >= 0)//validation
+            {
+                destination = new Coordinate(i - ei, j);
+                if (BoardMaker.theBoard.getSquare(destination).getPiece() == Piece.EMPTY) {
+                    if (MoveValidation.isThisMoveValid(source, destination))
+                        result.add(destination);
+                }
+                else {
+                    if(BoardMaker.theBoard.getSquare(destination).getPieceColor() != square.getPieceColor()) {
+                        if (MoveValidation.isThisMoveValid(source, destination))
+                            result.add(destination);
+                    }
+                    break;
+                }
+            } else {
+                break;
+            }
+        }   //down moves
+        for (int ei = 1; ei < 8; ei++) {
+            if (i + ei < 8)//validation
+            {
+                destination = new Coordinate(i + ei, j);
+                if (BoardMaker.theBoard.getSquare(destination).getPiece() == Piece.EMPTY) {
+                    if (MoveValidation.isThisMoveValid(source, destination))
+                        result.add(destination);
+                }
+                else {
+                    if(BoardMaker.theBoard.getSquare(destination).getPieceColor() != square.getPieceColor()) {
+                        if (MoveValidation.isThisMoveValid(source, destination))
+                            result.add(destination);
+                    }
+                    break;
+                }
+            } else {
+                break;
+            }
+        }
+        return result;
     }
 }
